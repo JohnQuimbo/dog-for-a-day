f52352ab5fcdea9e667416d191bbf697a9fce751
mwaller4 <55846102+mwaller4@users.noreply.github.com>
2020-03-25 17:35:50 -0400

    Add files via upload

--- seeingEyeDog.sql
+++ seeingEyeDog.sql
@@ -21,4 +21,33 @@ CREATE TABLE family (
     email             varChar(25),
     primary key(id#),
     foreign key(dog) references dogs(id#)
-);    
+); 
+
+--DROP TABLE trainer CASCADE CONSTRAINTS
+CREATE TABLE trainer (
+    id#             number(5)   not null,
+    first_name      varchar(25) not null,
+    last_name       varchar(25) not null,
+    address         varchar(25) not null,
+    phone_number    varChar(11) not null,
+    email           varChar(25),
+    dogs_training   number(1),
+    salary number(7),
+    primary key(id#)
+    );
+
+--DROP TABLE blind_person CASCADE CONSTRAINTS
+CREATE TABLE blind_person (
+    id#             number(5)   not null,
+    first_name      varchar(25) not null,
+    last_name       varchar(25) not null,
+    address         varchar(25) not null,
+    phone_number    varChar(11) not null,
+    email           varChar(25),
+    wait_list_time  varchar(30), 
+    dog_assigned    number(5),
+    primary key(id#),
+    foreign key(dog_assigned) references dogs(id#)
+    );
+    
+    
